6912e30280b9500821b76afbade53214a9875b76
Ayatooo <louisreynard919@gmail.com>
2022-10-06 15:55:50 +0200

    :zap: faker done

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -5,25 +5,29 @@ namespace App\DataFixtures;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use App\Entity\Restaurant;
+use Faker\Generator;
+use Faker\Factory;
 
 class AppFixtures extends Fixture
 {
+    private Generator $faker;
+
+    public function __construct()
+    {
+        $this->faker = Factory::create("fr_FR");
+    }
+
     public function load(ObjectManager $manager): void
     {
-
+        
         for ($i = 0; $i < 100; $i++) {
             $restaurant = new Restaurant();
-            $restaurant->setRestaurantName('Restaurant'.$i);
-            $restaurant->setRestaurantDescription(
-                'Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                Nulla vitae elit libero, a pharetra augue.'
-            );
-            $restaurant->setRestaurantLatitude((string) rand(0, 100) . '.' . rand(0, 100));
-            $restaurant->setRestaurantLongitude((string) rand(0, 100) . '.' . rand(0, 100));
-            $restaurant->setRestaurantPhone(
-                '0' . rand(0, 9) . rand(0, 9) . rand(0, 9) . rand(0, 9) . rand(0, 9) . rand(0, 9) . rand(0, 9) . rand(0, 9) . rand(0, 9) . rand(0, 9)
-            );
-            $restaurant->setStatus((bool) rand(0, 1));
+            $restaurant->setRestaurantName($this->faker->company())
+                ->setRestaurantDescription($this->faker->text(10))
+                ->setRestaurantLatitude($this->faker->latitude())
+                ->setRestaurantLongitude($this->faker->longitude())
+                ->setRestaurantPhone($this->faker->optional($weight= 0.8)->phoneNumber())
+                ->setStatus(rand(0, 1));
             $manager->persist($restaurant);
         }
         $manager->flush();
