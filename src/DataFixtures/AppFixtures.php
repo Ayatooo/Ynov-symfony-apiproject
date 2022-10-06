6bf98d96de495604c56b9ae2f844642b35a73200
Ayatooo <louisreynard919@gmail.com>
2022-10-06 14:58:56 +0200

    :zap: fixtures in progress

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,16 +4,28 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use App\Entity\Restaurant;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
 
-    //create 20 fake restaurants        
-
+        for ($i = 0; $i < 100; $i++) {
+            $restaurant = new Restaurant();
+            $restaurant->setRestaurantName('Restaurant'.$i);
+            $restaurant->setRestaurantDescription(
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                Nulla vitae elit libero, a pharetra augue.'
+            );
+            $restaurant->setRestaurantLatitude((string) rand(0, 100) . '.' . rand(0, 100));
+            $restaurant->setRestaurantLongitude((string) rand(0, 100) . '.' . rand(0, 100));
+            $restaurant->setRestaurantPhone(
+                '0' . rand(0, 9) . rand(0, 9) . rand(0, 9) . rand(0, 9) . rand(0, 9) . rand(0, 9) . rand(0, 9) . rand(0, 9) . rand(0, 9) . rand(0, 9)
+            );
+            $restaurant->setStatus((bool) rand(0, 1));
+            $manager->persist($restaurant);
+        }
         $manager->flush();
     }
 }
