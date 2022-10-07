2b28d0b03a9e036f06c5a230cc3b9146c4b48f01
Ayatooo <louisreynard919@gmail.com>
2022-10-07 15:37:51 +0200

    :bug: deleteOneUser constraint key need fix

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -2,12 +2,15 @@
 
 namespace App\Controller;
 
+use App\Entity\Users;
 use App\Repository\UsersRepository;
-use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 
 class UsersController extends AbstractController
 {
@@ -28,4 +31,19 @@ class UsersController extends AbstractController
         return new JsonResponse($data, Response::HTTP_OK, [], true);
     }
 
+    #[Route('/api/users/{idUsers}', name: 'users.getOne', methods: ['GET'])]
+    #[ParamConverter('users', options: ['id' => 'idUsers'])]
+    public function getOneRestaurant(Users $users, SerializerInterface $serializer): JsonResponse
+    {
+        return new JsonResponse($serializer->serialize($users, 'json', ['groups' => 'showUsers']), Response::HTTP_OK, [], true);
+    }
+
+    #[Route('/api/users/{idUsers}', name: 'users.delete', methods: ['DELETE'])]
+    #[ParamConverter('users', options: ['id' => 'idUsers'])]
+    public function deleteRestaurant(Users $users, EntityManagerInterface $entityManager): JsonResponse
+    {
+        $entityManager->remove($users);
+        $entityManager->flush();
+        return new JsonResponse(null, Response::HTTP_NO_CONTENT, [], false);
+    }
 }
