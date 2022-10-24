ec3a72a5d1310656afa1291bb476380eb8b2a155
Ayatooo <louisreynard919@gmail.com>
2022-10-24 09:36:36 +0200

    :zap: exception in progress

---
+++ src/EventSubscriber/ExceptionSubscriber.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+class ExceptionSubscriber implements EventSubscriberInterface
+{
+    public function onKernelException(ExceptionEvent $event): void
+    {
+        $exception = $event->getThrowable();
+        if ($exception instanceof \Exception) {
+            $event->setResponse(new JsonResponse([
+                "error" => "Quelque chose s'est mal passé",
+            ]));
+        } else {
+            $data = [
+                'status' => 500,
+                'message' => $exception->getMessage(),
+            ];
+            $event->setResponse(new JsonResponse($data));
+        }
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            KernelEvents::EXCEPTION => 'onKernelException',
+        ];
+    }
+}
