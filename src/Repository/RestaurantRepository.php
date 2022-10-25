4bfd8512253d6a96f0119001e3ad74a0c644d244
romainsilvy <romain.silvy06@gmail.com>
2022-10-25 12:08:43 +0200

    y a rien qui marche

--- src/Repository/RestaurantRepository.php
+++ src/Repository/RestaurantRepository.php
@@ -3,8 +3,10 @@
 namespace App\Repository;
 
 use App\Entity\Restaurant;
-use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\Query\ResultSetMapping;
 use Doctrine\Persistence\ManagerRegistry;
+use Doctrine\ORM\Query\ResultSetMappingBuilder;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 
 /**
  * @extends ServiceEntityRepository<Restaurant>
@@ -77,10 +79,33 @@ class RestaurantRepository extends ServiceEntityRepository
             ->getResult();
     }
 
-    public function findClosestRestaurant($latitude, $longitude, $distance) {
-        $sql = "SELECT *, ( 6371 * acos( cos( radians(:latitude) ) * cos( radians( latitude ) ) * cos( radians( longitude ) - radians(:longitude) ) + sin( radians(:latitude) ) * sin( radians( latitude ) ) ) ) AS distance FROM restaurant HAVING distance < :distance ORDER BY distance LIMIT 0 , 20";
-        $stmt = $this->getEntityManager()->getConnection()->prepare($sql);
-        $stmt->execute(['latitude' => $latitude, 'longitude' => $longitude, 'distance' => $distance]);
-        return $stmt->fetchAll();
+    public function findClosestRestaurant($latitude, $longitude, $distance)
+    {
+        // $conn = $this->getEntityManager()->getConnection();
+        // $sql = "SELECT *, ( 6371 * acos( cos( radians(:latitude) ) * cos( radians( restaurant_latitude ) ) * cos( radians( restaurant_longitude ) - radians(:longitude) ) + sin( radians(:latitude) ) * sin( radians( restaurant_latitude ) ) ) ) AS distance FROM restaurant HAVING distance < :distance ORDER BY distance LIMIT 0 , 20";
+        // $stmt = $conn->prepare($sql);
+        // $stmt->executeQuery(['latitude' => $latitude, 'longitude' => $longitude, 'distance' => $distance]);
+
+        // return $stmt->fetchAll();
+
+        $rsm = new ResultSetMapping;
+        $entityManager = $this->getEntityManager();
+        $rsm = new ResultSetMappingBuilder($entityManager);
+        $rsm->addRootEntityFromClassMetadata('App\Entity\Restaurant', 'r');
+
+        // $rsm->addEntityResult('App\Entity\Restaurant', 'r');
+        // $rsm->addFieldResult('r', 'id', 'id');
+        // $rsm->addFieldResult('r', 'restaurant_name', 'restaurantName');
+        // $rsm->addFieldResult('r', 'restaurant_description', 'restaurant_description');
+        // $rsm->addFieldResult('r', 'restaurant_latitude', 'restaurant_latitude');
+        // $rsm->addFieldResult('r', 'restaurant_longitude', 'restaurant_longitude');
+        // $rsm->addFieldResult('r', 'restaurant_address', 'restaurant_address');
+        // $rsm->addFieldResult('r', 'restaurant_phone', 'restaurant_phone');
+
+        $query = $this->_em->createNativeQuery('SELECT id FROM restaurant', $rsm);
+        // $query->setParameter(1, 'romanb');
+
+        $users = $query->getResult();
+        return $users;
     }
 }
